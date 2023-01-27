141d9ac4c1296b40a94800dbbfc41589c98cd608
to_be_peace_da <nikita.nik.u@mail.ru>
2023-01-27 15:53:22 +1000

    add sections and img block to news.show

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Advertisement;
 use App\Models\News;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
+//        $sections = Section::tree()->get()->toTree();
         $news = News::latest('id')->take(3)->get();
         $advertisements = Advertisement::latest('id')->take(5)->get();
 
