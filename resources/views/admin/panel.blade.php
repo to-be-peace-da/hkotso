b527d1b211cac763683212bacafd02ec2005f6df
to_be_peace_da <nikita.nik.u@mail.ru>
2023-04-20 18:04:50 +1000

    new admin interface

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -3,27 +3,90 @@
         <div class="container">
             <div class="admin-panel">
                 <div class="admin-options">
-                    <a href="{{ route('admin.news-create') }}">
-                        <h2>Создать новость</h2>
-                    </a>
-                    <a href="{{ route('admin.advertisement-create') }}">
-                        <h2>Создать объявление</h2>
-                    </a>
-                    <a href="{{ route('admin.schedule-create') }}">
-                        <h2>Добавить расписание</h2>
-                    </a>
-                    <a href="{{ route('admin.substitution-create') }}">
-                        <h2>Добавить замену</h2>
-                    </a>
-                    <a href="{{ route('admin.group-create') }}">
-                        <h2>Добавить группу</h2>
-                    </a>
-                    <a href="{{ route('admin.subject-create') }}">
-                        <h2>Добавить предмет</h2>
-                    </a>
-                    <a href="{{ route('admin.teacher-create') }}">
-                        <h2>Добавить преподавателя</h2>
-                    </a>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.news-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/news.svg') }}" alt="news">
+                                </div>
+                                <div class="title">
+                                    <h2>Создать новость</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.advertisement-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/advertisement.svg') }}" alt="advertisement">
+                                </div>
+                                <div class="title">
+                                    <h2>Создать объявление</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.schedule-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/schedule.svg') }}" alt="schedule">
+                                </div>
+                                <div class="title">
+                                    <h2>Добавить расписание</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.substitution-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/substitution.svg') }}" alt="substitution">
+                                </div>
+                                <div class="title">
+                                    <h2>Добавить замену</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.group-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/group.svg') }}" alt="group">
+                                </div>
+                                <div class="title">
+                                    <h2>Добавить группу</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.subject-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/subject.svg') }}" alt="subject">
+                                </div>
+                                <div class="title">
+                                    <h2>Добавить предмет</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="tool-wrapper">
+                        <a href="{{ route('admin.teacher-create') }}">
+                            <div class="tool">
+                                <div class="icon">
+                                    <img src="{{ asset('storage/images/teacher.svg') }}" alt="teacher">
+                                </div>
+                                <div class="title">
+                                    <h2>Добавить преподавателя</h2>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
