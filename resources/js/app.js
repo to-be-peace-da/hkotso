ada9a34b8ab6c20d13bb5972e4d0fec803bfa35e
to_be_peace_da <nikita.nik.u@mail.ru>
2023-05-04 23:42:23 +1000

    NEW SCHEDULE UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+const oddElements = document.querySelectorAll('.substitution-date');
+oddElements.forEach(function (e) {
+    const oddParent = e.closest('.odd');
+    if (oddParent) {
+        oddParent.classList.add('extra-padding-for-odd');
+    }
+});
--- resources/views/schedule/show.blade.php
+++ resources/views/schedule/show.blade.php
@@ -1,77 +1,196 @@
+@php use Carbon\Carbon; @endphp
 <x-layout>
     <div class="even">
-        <div class="container">
-            <div class="schedule">
+        <div class="schedule-and-substitution">
+            <div class="container">
                 <div class="title">
                     <h1>{{ $schedules->first()->group->name . " - " . $schedules->first()->department->name . " - " . $schedules->first()->course->name . " курс" }}</h1>
                 </div>
-                @foreach($days as $day)
-                    <div class="day">
-                        <h2>{{ $day->name }}</h2>
+            </div>
+            @foreach($days as $day)
+                <div class="even">
+                    <div class="container">
+                        <div class="day">
+                            <h2>{{ $day->name }}</h2>
+                        </div>
                     </div>
-                    @php
-                        $substitutionShowed = false;
-                    @endphp
-                    @foreach($substitutions->where('day_id', '=', $day->id) as $substitution)
-                        @if($substitutionShowed === false)
-                            <p>Замена на {{ \Carbon\Carbon::create($substitution->date)->format('d.m.Y') }}</p>
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <td>№</td>
-                                    <td>Пара</td>
-                                    <td>Преподаватель</td>
-                                    <td>Кабинет</td>
-                                </tr>
-                                </thead>
+                </div>
+                @php
+                    $substitutionShowed = false;
+                @endphp
+                <div class="substitution">
+                    <div class="odd">
+                        <div class="container">
+                            @foreach($substitutions->where('day_id', '=', $day->id) as $substitution)
+                                @if($substitutionShowed === false)
+                                    <div class="substitution-date">
+                                        <p>Замена на {{ Carbon::create($substitution->date)->format('d.m.Y') }}</p>
+                                    </div>
+                                    <div class="substitution-head">
+                                        <div class="items">
+                                            <div class="item">
+                                                <p>№</p>
+                                            </div>
+                                            <div class="item">
+                                                <p>Пара</p>
+                                            </div>
+                                            <div class="item">
+                                                <p>Преподаватель</p>
+                                            </div>
+                                            <div class="item">
+                                                <p>Кабинет</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 @endif
                                 @php
                                     $substitutionShowed = true;
                                 @endphp
-                                <tbody>
                                 @if($substitution->day->id === $day->id)
-                                    <tr>
-                                        <td>{{ $substitution->order->name }}</td>
-                                        <td>{{ $substitution->subject->name }}</td>
-                                        <td>
-                                            <a href="{{ route('teacher.show', $substitution->teacher) }}">{{ $substitution->teacher->surname .  " " . $substitution->teacher->name . ". " . $substitution->teacher->patronymic . "." }}</a>
-                                        </td>
-                                        <td>{{ $substitution->audience->number }}</td>
-                                    </tr>
+                                    <div class="substitution-items">
+                                        <div class="items">
+                                            <div class="item">
+                                                <p>{{ $substitution->order->name }}</p>
+                                            </div>
+                                            <div class="item">
+                                                <p>{{ $substitution->subject->name }}</p>
+                                            </div>
+                                            <div class="item">
+                                                <a href="{{ route('teacher.show', $substitution->teacher) }}"
+                                                   target="_blank">{{ $substitution->teacher->surname .  " " . $substitution->teacher->name . ". " . $substitution->teacher->patronymic . "." }}</a>
+                                            </div>
+                                            <div class="item">
+                                                <p>{{ $substitution->audience->number }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 @endif
-                                </tbody>
-                                @endforeach
-                            </table>
 
-                            <p>=============================================================================</p>
-
-                            <table class="table">
-                                <thead>
-                                <tr>
-                                    <td>№</td>
-                                    <td>Пара</td>
-                                    <td>Преподаватель</td>
-                                    <td>Кабинет</td>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($schedules->sortBy('order_id') as $schedule)
-                                    @if($schedule->day->id === $day->id)
-                                        <tr>
-                                            <td>{{ $schedule->order->name }}</td>
-                                            <td>{{ $schedule->subject->name }}</td>
-                                            <td>
-                                                <a href="{{ route('teacher.show', $schedule->teacher) }}">{{ $schedule->teacher->surname .  " " . $schedule->teacher->name . " " . $schedule->teacher->patronymic . " " }}</a>
-                                            </td>
-                                            <td>{{ $schedule->audience->number }}</td>
-                                        </tr>
-                                    @endif
-                                @endforeach
-                                </tbody>
-                            </table>
                             @endforeach
-            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="schedule">
+                    <div class="even">
+                        <div class="container">
+                            <div class="schedule-head">
+                                <div class="items">
+                                    <div class="item">
+                                        <p>№</p>
+                                    </div>
+                                    <div class="item">
+                                        <p>Пара</p>
+                                    </div>
+                                    <div class="item">
+                                        <p>Преподаватель</p>
+                                    </div>
+                                    <div class="item">
+                                        <p>Кабинет</p>
+                                    </div>
+                                </div>
+                            </div>
+                            @foreach($schedules->sortBy('order_id') as $schedule)
+                                @if($schedule->day->id === $day->id)
+                                    <div class="schedule-items">
+                                        <div class="items">
+                                            <div class="item">
+                                                <p>{{ $schedule->order->name }}</p>
+                                            </div>
+                                            <div class="item">
+                                                <p>{{ $schedule->subject->name }}</p>
+                                            </div>
+                                            <div class="item">
+                                                <a href="{{ route('teacher.show', $schedule->teacher) }}"
+                                                   target="_blank">{{ $schedule->teacher->surname .  " " . $schedule->teacher->name . " " . $schedule->teacher->patronymic . " " }}</a>
+                                            </div>
+                                            <div class="item">
+                                                <p>{{ $schedule->audience->number }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </x-layout>
 
+{{--<x-layout>--}}
+{{--    <div class="even">--}}
+{{--        <div class="container">--}}
+{{--            <div class="schedule">--}}
+{{--                <div class="title">--}}
+{{--                    <h1>{{ $schedules->first()->group->name . " - " . $schedules->first()->department->name . " - " . $schedules->first()->course->name . " курс" }}</h1>--}}
+{{--                </div>--}}
+{{--                @foreach($days as $day)--}}
+{{--                    <div class="day">--}}
+{{--                        <h2>{{ $day->name }}</h2>--}}
+{{--                    </div>--}}
+{{--                    @php--}}
+{{--                        $substitutionShowed = false;--}}
+{{--                    @endphp--}}
+{{--                    @foreach($substitutions->where('day_id', '=', $day->id) as $substitution)--}}
+{{--                        @if($substitutionShowed === false)--}}
+{{--                            <p>Замена на {{ \Carbon\Carbon::create($substitution->date)->format('d.m.Y') }}</p>--}}
+{{--                            <table class="table">--}}
+{{--                                <thead>--}}
+{{--                                <tr>--}}
+{{--                                    <td>№</td>--}}
+{{--                                    <td>Пара</td>--}}
+{{--                                    <td>Преподаватель</td>--}}
+{{--                                    <td>Кабинет</td>--}}
+{{--                                </tr>--}}
+{{--                                </thead>--}}
+{{--                                @endif--}}
+{{--                                @php--}}
+{{--                                    $substitutionShowed = true;--}}
+{{--                                @endphp--}}
+{{--                                <tbody>--}}
+{{--                                @if($substitution->day->id === $day->id)--}}
+{{--                                    <tr>--}}
+{{--                                        <td>{{ $substitution->order->name }}</td>--}}
+{{--                                        <td>{{ $substitution->subject->name }}</td>--}}
+{{--                                        <td>--}}
+{{--                                            <a href="{{ route('teacher.show', $substitution->teacher) }}">{{ $substitution->teacher->surname .  " " . $substitution->teacher->name . ". " . $substitution->teacher->patronymic . "." }}</a>--}}
+{{--                                        </td>--}}
+{{--                                        <td>{{ $substitution->audience->number }}</td>--}}
+{{--                                    </tr>--}}
+{{--                                @endif--}}
+{{--                                </tbody>--}}
+{{--                                @endforeach--}}
+{{--                            </table>--}}
+
+{{--                            <p>=============================================================================</p>--}}
+
+{{--                            <table class="table">--}}
+{{--                                <thead>--}}
+{{--                                <tr>--}}
+{{--                                    <td>№</td>--}}
+{{--                                    <td>Пара</td>--}}
+{{--                                    <td>Преподаватель</td>--}}
+{{--                                    <td>Кабинет</td>--}}
+{{--                                </tr>--}}
+{{--                                </thead>--}}
+{{--                                <tbody>--}}
+{{--                                @foreach($schedules->sortBy('order_id') as $schedule)--}}
+{{--                                    @if($schedule->day->id === $day->id)--}}
+{{--                                        <tr>--}}
+{{--                                            <td>{{ $schedule->order->name }}</td>--}}
+{{--                                            <td>{{ $schedule->subject->name }}</td>--}}
+{{--                                            <td>--}}
+{{--                                                <a href="{{ route('teacher.show', $schedule->teacher) }}">{{ $schedule->teacher->surname .  " " . $schedule->teacher->name . " " . $schedule->teacher->patronymic . " " }}</a>--}}
+{{--                                            </td>--}}
+{{--                                            <td>{{ $schedule->audience->number }}</td>--}}
+{{--                                        </tr>--}}
+{{--                                    @endif--}}
+{{--                                @endforeach--}}
+{{--                                </tbody>--}}
+{{--                            </table>--}}
+{{--                            @endforeach--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</x-layout>--}}
